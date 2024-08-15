3a9bee2e7e24e23659d7ffe9e501eed0f3428efc
sajib689 <jibonraja11@gmail.com>
2024-08-15 17:47:00 +0600

    server add

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 8000
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+app.use(express.json())
+app.use(cors())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.2m0rny5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    
+    } finally {
+   
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the e-store server')
+})
+
+app.listen(port, () => {
+    console.log(`Listening on ${port}`)
+})
+
